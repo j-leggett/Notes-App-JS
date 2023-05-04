eb62b1f711f173db301f4ac7d4a1755ca197d3d7
Jack <jwleggett@outlook.com>
2023-05-04 14:59:18 +0100

    added in fuctionality to display notes on webpage

--- index.js
+++ index.js
@@ -1,5 +1,13 @@
 const NotesModel = require('./notesModel')
+const NotesView = require('./displayNotes')
+
+// setup the model
 const model = new NotesModel();
+model.addNote('This is an example note')
+
+// setup the view
+const view = new NotesView(model);
+
+//make the view display notes
+view.displayNotes()
 console.log('The notes app is running');
-model.addNote('Buy milk');
-console.log(model.getNotes());
